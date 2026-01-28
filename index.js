7b8f978f2cf06970600e88b07e918f3daab28b4a
19Bas2y8a-Nik <19Bas2y8a@mail.ru>
2026-01-28 17:17:54 +0300

    Enhance bot response with random greetings instead of a static message.

--- index.js
+++ index.js
@@ -14,13 +14,19 @@ const bot = new TelegramBot(token, { polling: true });
 
 console.log('Бот запущен и готов к работе!');
 
+// Массив приветствий
+const greetings = ['Добрый день', 'Привет', 'Здравствуйте'];
+
 // Обработчик всех текстовых сообщений
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   const messageText = msg.text;
 
+  // Случайным образом выбираем приветствие
+  const randomGreeting = greetings[Math.floor(Math.random() * greetings.length)];
+  
   // Отвечаем на любое сообщение
-  bot.sendMessage(chatId, 'Привет, я бот!');
+  bot.sendMessage(chatId, `${randomGreeting}, я бот!`);
 });
 
 // Обработчик ошибок
